1d091302cf7b699ce7958df2d347a4b2eae9c6af
Sirapob <blacking987@gmail.com>
2024-01-29 21:38:23 +0700

    Header 0.1

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="flex bg-gradient-to-b from-gray-400 to-white">
+    <img src="./assets/Logo.png" alt="" class="w-16 rotate-60 ml-3" />
+    <img src="./assets/font.png" alt="" class="w-16 h-10 mt-4 ml-3" />
+    <div class="flex-grow"></div>
+    <!-- This div will push the toggle button to the right -->
+    <input type="checkbox" class="toggle h-8 mt-4 mr-3" checked />
+    <!-- Moved to the right side -->
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
