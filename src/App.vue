f66915c698af503fa9a9e62f1221da285c4a04a3
Sirapob <blacking987@gmail.com>
2024-02-07 13:51:23 +0700

    Update UI Add Methonome

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,39 @@ import { ref } from 'vue'
 const isClick = ref(false)
 const theKey = ref([])
 const checkKey = function (key) {
-  theKey.value += key
+  if (theKey.value.length < 18) {
+    theKey.value += key
+  } else {
+    theKey.value = []
+  }
+}
+const getNoteColor = (note) => {
+  switch (note) {
+    case 'C':
+      return '#F9EFDB'
+    case 'C#':
+      return '#EBD9B4'
+    case 'D':
+      return '#9DBC98'
+    case 'D#':
+      return '#638889'
+    case 'E':
+      return '#D9EDBF'
+    case 'F':
+      return '#FFB996'
+    case 'F#':
+      return '#FFCF81'
+    case 'G':
+      return '#FDFFAB'
+    case 'G#':
+      return '#FDF7E4'
+    case 'A':
+      return '#FAEED1'
+    case 'A#':
+      return '#DED0B6'
+    case 'B':
+      return '#BBAB8C'
+  }
 }
 </script>
 <script>
@@ -26,6 +58,8 @@ export default {
         'A#',
         'B',
       ],
+      isButtonClickSoundPlaying: false,
+      buttonClickAudio: new Audio('/src/assets/youtube_ymJIXzvDvj4_audio.mp3'),
     }
   },
   methods: {
@@ -52,6 +86,18 @@ export default {
       const noteDistance =
         this.pianoKeys.indexOf(note) - this.pianoKeys.indexOf('A')
       return A4Frequency * Math.pow(2, noteDistance / 12)
+    },
+    toggleMetronome() {
+      if (this.isButtonClickSoundPlaying) {
+        // If the sound is currently playing, stop it
+        this.buttonClickAudio.pause()
+        this.isButtonClickSoundPlaying = false
+      } else {
+        // If the sound is not playing, start it
+        this.buttonClickAudio.currentTime = 0 // Reset the audio to the beginning
+        this.buttonClickAudio.play()
+        this.isButtonClickSoundPlaying = true
+      }
     },
   },
 }
@@ -98,9 +144,34 @@ export default {
       @click="isClick = !isClick"
       class="fa-solid fa-circle-info fixed bottom-0 right-0 fa-2x m-2 cursor-pointer text-gray-500 hover:text-gray-700"
     ></i>
+    <!-- Note press showing -->
+    <div
+      :class="isClick ? 'hidden' : ''"
+      class="flex justify-center text-center"
+      style="margin-top: 5rem"
+    >
+      <div class="w-1/6 bg-white bg-opacity-10 mx-auto p-5">
+        <span class="note">
+          {{ theKey }}
+        </span>
+      </div>
+    </div>
+
+    <!-- Metro-->
+    <div>
+      <button
+        @click="toggleMetronome"
+        class="fixed bottom-0 right-10 p-2 bg-slate-500 text-white rounded-full cursor-pointer hover:bg-slate-700"
+      >
+        {{ isButtonClickSoundPlaying ? 'Stop Metronome' : 'Start Metronome' }}
+      </button>
+    </div>
+
     <!-- Piano Section -->
-    {{ theKey }}
-    <div class="flex justify-center absolute top-96">
+    <div
+      :class="isClick ? 'hidden' : ''"
+      class="flex justify-center absolute top-56"
+    >
       <div class="w-3/6">
         <div
           v-for="note in pianoKeys"
@@ -108,7 +179,8 @@ export default {
           @mousedown="playSound(note)"
           @mouseup="stopSound"
           @click="checkKey(note)"
-          class="piano-key mt-5"
+          :style="{ backgroundColor: getNoteColor(note) }"
+          class="piano-key mt-4"
         >
           {{ note }}
         </div>
@@ -122,16 +194,22 @@ export default {
 .piano-key {
   width: 100px;
   height: 100px;
-  border: 1px solid #000;
   display: inline-block;
   text-align: center;
   line-height: 150px;
   cursor: pointer;
   margin-right: 20px;
   user-select: none;
+  color: #ffff;
+  font-family: 'Protest Riot', sans-serif;
+}
+.note {
+  font-family: 'Protest Riot', sans-serif;
+  color: #ffff;
 }
 
 .piano-key:active {
   background-color: #ddd;
+  border: 2px solid red;
 }
 </style>
