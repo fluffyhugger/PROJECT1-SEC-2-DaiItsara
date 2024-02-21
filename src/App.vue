6d8db6f5e6d31552a9ef9b6266cd5ffc5d18c6db
Nisarat Aunrueanngam <noansrnn@gmail.com>
2024-02-22 01:16:19 +0700

    mv css of piano and trap

--- src/App.vue
+++ src/App.vue
@@ -205,101 +205,47 @@ const metroPopupOpen = ref(false);
     </div>
     <!-- SVG THEME -->
     <div>
-      <button
-        @click="togglePopup"
-        class="fixed bottom-1 right-20 p-1.5 bg-white text-white rounded-full cursor-pointer hover:bg-slate-700"
-      >
-        <img
-          src="./components/icons/paint-brush.svg"
-          style="width: 20px; height: 20px"
-        />
+      <button @click="togglePopup"
+        class="fixed bottom-1 right-20 p-1.5 bg-white text-white rounded-full cursor-pointer hover:bg-slate-700">
+        <img src="./components/icons/paint-brush.svg" style="width: 20px; height: 20px" />
       </button>
     </div>
-    <div
-      v-if="popupOpen"
-      style="z-index: 100"
-      class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50"
-    >
+    <div v-if="popupOpen" style="z-index: 100"
+      class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
       <div class="bg-white rounded-lg p-8 relative">
         <div class="absolute top-2 right-2 cursor-pointer" @click="closePopup">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </div>
         <div class="grid grid-cols-3 gap-4">
-          <img
-            src="./assets/theme-1.png"
-            class="theme-option rounded-full cursor-pointer"
-            @click="selectTheme(1)"
-          />
-          <img
-            src="./assets/theme-2.png"
-            class="theme-option rounded-full cursor-pointer"
-            @click="selectTheme(2)"
-          />
-          <img
-            src="./assets/theme-3.png"
-            class="theme-option rounded-full cursor-pointer"
-            @click="selectTheme(3)"
-          />
+          <img src="./assets/theme-1.png" class="theme-option rounded-full cursor-pointer" @click="selectTheme(1)" />
+          <img src="./assets/theme-2.png" class="theme-option rounded-full cursor-pointer" @click="selectTheme(2)" />
+          <img src="./assets/theme-3.png" class="theme-option rounded-full cursor-pointer" @click="selectTheme(3)" />
         </div>
       </div>
     </div>
     <!-- Metro PopUp -->
-    <div
-      v-if="metroPopupOpen"
-      style="z-index: 100"
-      class="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50"
-    >
+    <div v-if="metroPopupOpen" style="z-index: 100"
+      class="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50">
       <div class="bg-white rounded-lg p-8 relative">
-        <div
-          class="absolute top-2 right-2 cursor-pointer"
-          @click="toggleMetroclosePopup"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
+        <div class="absolute top-2 right-2 cursor-pointer" @click="toggleMetroclosePopup">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </div>
         <!-- Select bpm speed -->
         <div class="grid grid-cols-3 gap-4">
-          <div
-            @click="toggleMetronome"
-            class="theme-option rounded-full cursor-pointer text-black bg-gray-300 bg-opacity-80 hover:bg-gray-500 text-center leading-10"
-          >
+          <div @click="toggleMetronome"
+            class="theme-option rounded-full cursor-pointer text-black bg-gray-300 bg-opacity-80 hover:bg-gray-500 text-center leading-10">
             60 bpm
           </div>
-          <div
-            @click="toggleMetronomeMedium"
-            class="theme-option rounded-full cursor-pointer text-black bg-gray-300 bg-opacity-80 hover:bg-gray-500 text-center leading-10"
-          >
+          <div @click="toggleMetronomeMedium"
+            class="theme-option rounded-full cursor-pointer text-black bg-gray-300 bg-opacity-80 hover:bg-gray-500 text-center leading-10">
             80 bpm
           </div>
-          <div
-            @click="toggleMetronomeFast"
-            class="theme-option rounded-full cursor-pointer text-black bg-gray-300 bg-opacity-80 hover:bg-gray-500 text-center leading-10"
-          >
+          <div @click="toggleMetronomeFast"
+            class="theme-option rounded-full cursor-pointer text-black bg-gray-300 bg-opacity-80 hover:bg-gray-500 text-center leading-10">
             100 bpm
           </div>
         </div>
@@ -310,14 +256,9 @@ const metroPopupOpen = ref(false);
     <section>
       <!-- UI for selecting oscillator type -->
       <div v-if="isActive" class="oscillator-mode mb-6 flex justify-center">
-        <label for="oscillator-type" class="text-white"
-          >Select Oscillator Type:</label
-        >
-        <select
-          v-model="selectedOscillatorType"
-          @change="changeOscillatorType"
-          class="ml-2 rounded-md bg-white bg-opacity-10 text-white"
-        >
+        <label for="oscillator-type" class="text-white">Select Oscillator Type:</label>
+        <select v-model="selectedOscillatorType" @change="changeOscillatorType"
+          class="ml-2 rounded-md bg-white bg-opacity-10 text-white">
           <option value="sine">Sine</option>
           <option value="square">Square</option>
           <option value="sawtooth">Sawtooth</option>
@@ -338,14 +279,8 @@ const metroPopupOpen = ref(false);
     <div>
       <!-- Switch piano section -->
       <div class="flex items-center justify-center mt-3" v-if="!showInfo">
-        <span class="text-white" :class="{ active: isActive }"
-          >Classic piano</span
-        >
-        <div
-          class="toggle-button"
-          :class="{ active: isActive }"
-          @click="toggle"
-        >
+        <span class="text-white" :class="{ active: isActive }">Classic piano</span>
+        <div class="toggle-button" :class="{ active: isActive }" @click="toggle">
           <div class="toggle-circle"></div>
         </div>
 
@@ -353,66 +288,33 @@ const metroPopupOpen = ref(false);
         <!-- Piano Volume Control Input-->
         <span class="ml-5" v-if="!isActive && !showInfo">
           <span class="flex justify-center text-white">
-            Volume: {{ volume }}</span
-          >
+            Volume: {{ volume }}</span>
           <div class="volume-control ml-2 mb-3">
-            <input
-              id="volume-slider"
-              type="range"
-              min="0"
-              max="1"
-              step="0.1"
-              v-model="volume"
-              @input="setVolume(volume)"
-              class="p-2 rounded-md bg-gray-800 text-white"
-            />
+            <input id="volume-slider" type="range" min="0" max="1" step="0.1" v-model="volume" @input="setVolume(volume)"
+              class="p-2 rounded-md bg-gray-800 text-white" />
           </div>
         </span>
         <!-- Oscillator Volume Control Input-->
         <span class="ml-5" v-if="isActive && !showInfo">
           <span class="flex justify-center text-white">
-            Volume: {{ trapVolume }}</span
-          >
+            Volume: {{ trapVolume }}</span>
           <div class="volume-control ml-2 mb-3">
-            <input
-              id="volume-slider"
-              type="range"
-              min="0"
-              max="1"
-              step="0.1"
-              v-model="trapVolume"
-              @input="setTrapVolume(trapVolume)"
-              class="p-2 rounded-md bg-gray-800 text-white"
-            />
+            <input id="volume-slider" type="range" min="0" max="1" step="0.1" v-model="trapVolume"
+              @input="setTrapVolume(trapVolume)" class="p-2 rounded-md bg-gray-800 text-white" />
           </div>
         </span>
       </div>
     </div>
 
     <!-- Trap Section -->
-    <section
-      v-show="isActive"
-      id="trap"
-      class="trap-section flex justify-center"
-      v-if="!showInfo"
-      @keydown="handleTrapKeyDown"
-      @keyup="handleTrapKeyUp"
-      tabindex="0"
-    >
+    <section v-show="isActive" id="trap" class="trap-section flex justify-center" v-if="!showInfo"
+      @keydown="handleTrapKeyDown" @keyup="handleTrapKeyUp" tabindex="0">
       <div class="flex justify-center">
         <div class="trap-container">
-          <div
-            v-for="trap in TrapKeys"
-            :key="trap"
-            @click="checkKey(trap)"
-            @mousedown="handleTrapMouseDown(trap)"
-            @mouseup="stopSound"
-            :style="{ backgroundColor: getNoteColor(trap) }"
-            :class="{
+          <div v-for="trap in TrapKeys" :key="trap" @click="checkKey(trap)" @mousedown="handleTrapMouseDown(trap)"
+            @mouseup="stopSound" :style="{ backgroundColor: getNoteColor(trap) }" :class="{
               'trap-key': true,
-            }"
-            tabindex="0"
-          >
+            }" tabindex="0">
             <!-- Add tabindex to make the div focus on keyboard events -->
             {{ trap }}
           </div>
@@ -420,71 +322,36 @@ const metroPopupOpen = ref(false);
       </div>
     </section>
     <!-- Piano Section -->
-    <section
-      id="piano"
-      class="flex justify-center"
-      @keyup="(event) => handleKeyUp(event, volume)"
-      @keydown="(event) => handlePianoKeyDown(event)"
-      tabindex="0"
-      v-if="!isActive && !showInfo"
-    >
-      <div
-        v-for="(note, key) in keyBindings"
-        :key="key"
-        :class="{
-          'piano-key': true,
-          'black-key': isBlackKey(note),
-        }"
-        :data-note="note"
-        class="rounded"
-        @click="() => handleClick(note, volume)"
-        @mouseup="checkKey(note)"
-      >
+    <section id="piano" class="flex justify-center" @keyup="(event) => handleKeyUp(event, volume)"
+      @keydown="(event) => handlePianoKeyDown(event)" tabindex="0" v-if="!isActive && !showInfo">
+      <div v-for="(note, key) in keyBindings" :key="key" :class="{
+        'piano-key': true,
+        'black-key': isBlackKey(note),
+      }" :data-note="note" class="rounded" @click="() => handleClick(note, volume)" @mouseup="checkKey(note)">
         {{ note }}
       </div>
     </section>
 
     <div class="flex justify-center p-10">
       <!-- Icon to toggle the visibility of the information section -->
-      <i
-        @click="toggleInfo"
-        class="fa-solid fa-circle-info fixed bottom-3 right-0 fa-2xl m-2 cursor-pointer text-white hover:text-gray-700"
-      ></i>
+      <i @click="toggleInfo"
+        class="fa-solid fa-circle-info fixed bottom-3 right-0 fa-2xl m-2 cursor-pointer text-white hover:text-gray-700"></i>
 
       <!-- Metro Section-->
       <div>
-        <button
-          @click="toggleMetronome"
-          class="fixed bottom-1 right-11 p-1.5 bg-white text-white rounded-full cursor-pointer hover:bg-slate-700"
-        >
-          <img
-            v-if="!isButtonClickSoundPlaying"
-            src="./components/icons/metronome.svg"
-            alt="Start Metronome"
-            style="width: 20px; height: 20px"
-          />
-          <img
-            v-else
-            src="./components/icons/metronome.svg"
-            alt="Stop Metronome"
-            style="width: 20px; height: 20px"
-          />
+        <button @click="toggleMetronome"
+          class="fixed bottom-1 right-11 p-1.5 bg-white text-white rounded-full cursor-pointer hover:bg-slate-700">
+          <img v-if="!isButtonClickSoundPlaying" src="./components/icons/metronome.svg" alt="Start Metronome"
+            style="width: 20px; height: 20px" />
+          <img v-else src="./components/icons/metronome.svg" alt="Stop Metronome" style="width: 20px; height: 20px" />
         </button>
       </div>
       <!-- Information section -->
-      <div
-        v-if="showInfo"
-        class="relative max-w-xl mx-auto mt-8 p-6 bg-white shadow-lg rounded-lg"
-      >
+      <div v-if="showInfo" class="relative max-w-xl mx-auto mt-8 p-6 bg-white shadow-lg rounded-lg">
         <!-- Cross icon to close the information section -->
-        <i
-          @click="toggleInfo"
-          class="fa-solid fa-times-circle absolute top-2 right-2 cursor-pointer text-gray-500 hover:text-gray-700"
-        ></i>
-        <h1
-          class="text-3xl font-semibold mb-4 text-red-400"
-          style="font-family: 'Roboto', sans-serif"
-        >
+        <i @click="toggleInfo"
+          class="fa-solid fa-times-circle absolute top-2 right-2 cursor-pointer text-gray-500 hover:text-gray-700"></i>
+        <h1 class="text-3xl font-semibold mb-4 text-red-400" style="font-family: 'Roboto', sans-serif">
           How to Play Piano
         </h1>
         <p class="text-gray-700" style="font-family: 'Roboto', sans-serif"></p>
@@ -508,9 +375,7 @@ const metroPopupOpen = ref(false);
           </li>
         </ol>
       </div>
-      <footer
-        class="font-l font-light text-white text-center fixed bottom-0 left-0 w-full"
-      >
+      <footer class="font-l font-light text-white text-center fixed bottom-0 left-0 w-full">
         @DAIISARA INT203 ClIENT SIDE II
       </footer>
     </div>
@@ -518,74 +383,6 @@ const metroPopupOpen = ref(false);
 </template>
 <style scoped>
 @import "./components/util.css";
+@import "./components/PianoTrap.css";
 
-.piano-key:active {
-  background-color: #ddd;
-  border: 2px solid gray;
-}
-
-.piano-key {
-  width: 4.5dvw;
-  height: 30dvh;
-  display: inline-block;
-  text-align: center;
-  line-height: 500px;
-  cursor: pointer;
-  user-select: none;
-  background-color: #ffff;
-  color: gray;
-  font-size: 1vw;
-  /* Responsive font size */
-  border: 1px solid #00000046;
-  border-radius: 8px;
-  color: #000000;
-}
-.black-key {
-  color: white;
-}
-
-.black-key {
-  width: 2.5vw;
-  z-index: 2;
-  line-height: 20px;
-  margin: 0 -1.8vw 0 -1.8vw;
-  height: 15vh;
-  border: 1px solid #000;
-  border-radius: 0 0 5px 5px;
-  background-color: #363636;
-}
-
-.piano-key:hover {
-  background-color: lightgray;
-}
-
-.trap-key {
-  width: 4.5vw;
-  height: 30vh;
-  display: inline-block;
-  text-align: center;
-  line-height: 30vh;
-  /* Center vertically */
-  cursor: pointer;
-  user-select: none;
-  background-color: #ffff;
-  color: gray;
-  font-size: 1vw;
-  /* Responsive font size */
-  border: 1px solid #00000046;
-  border-radius: 8px;
-  margin: 0 auto;
-  /* Center horizontally */
-}
-
-.trap-section {
-  /* Adjust as needed */
-  padding: 20px;
-}
-
-.trap-container {
-  /* Adjust as needed */
-  max-width: 1200px;
-  width: 100%;
-}
 </style>
